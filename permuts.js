8c067a0e5cb0cf03c738429453c44b30f07ae358
Neil Gewirtz <ngewirtz86@gmail.com>
2018-04-18 22:21:47 -0400

    perms

---
+++ permuts.js
@@ -0,0 +1,18 @@
+function permutations(nums) {
+
+  if (nums.length <= 1) {
+    return [nums];
+  }
+
+  let first = nums[0];
+  let perms = permutations(nums.slice(1));
+  let totalPerms = [];
+  perms.forEach(perm => {
+    for (let i = 0; i <= perm.length; i++) {
+      totalPerms.push(perm.slice(0, i) + [first] + perm.slice(i));
+    }
+  });
+  return totalPerms;
+}
+
+console.log(permutations([1,2,3]));
