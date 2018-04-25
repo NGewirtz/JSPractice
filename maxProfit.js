5cd2ca121c64dd875c5dada54e46a083997b9523
Neil Gewirtz <ngewirtz86@gmail.com>
2018-04-25 17:27:03 -0400

    max profit

---
+++ maxProfit.js
@@ -0,0 +1,12 @@
+function findMaxProfit(prices) {
+  let minBuy = prices[0];
+  let maxProfit = 0;
+  prices.slice(1).forEach(price => {
+    if(price < minBuy) {
+        minBuy = price;
+    }else if(price - minBuy > maxProfit) {
+        maxProfit = price - minBuy;
+    }
+  });
+  return maxProfit;
+}
