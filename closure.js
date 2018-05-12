c287c9d3445a1c9f6b16490393b95ef503658970
Neil Gewirtz <ngewirtz86@gmail.com>
2018-05-12 16:57:44 -0400

    and or questions

--- closure.js
+++ closure.js
@@ -16,17 +16,3 @@ let passwordProtectedSecret = takeSecret("I like doritos");
 console.log(passwordProtectedSecret);
 console.log(passwordProtectedSecret("sadsf"));
 console.log(passwordProtectedSecret("password"));
-
-
-
-console.log("0 || 1 = "+(0 || 1));
-console.log("1 || 2 = "+(1 || 2));
-console.log("0 && 1 = "+(0 && 1));
-console.log("1 && 2 = "+(1 && 2));
-
-
-
-// 1
-// 1
-// 0
-// 2
