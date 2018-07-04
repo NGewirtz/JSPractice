9d0d6a6c729e10894a6d864271b11c7d5c00a34f
Neil Gewirtz <ngewirtz86@gmail.com>
2018-07-04 13:16:01 -0400

    ammend

---
+++ ammend.rb
@@ -0,0 +1,12 @@
+def amendTheSentence(s)
+    str = ""
+    caps = ("A".."Z").to_a
+    s[1..-1].chars.each do |char|
+        if caps.include?(char)
+            str << " #{char.downcase}"
+        else
+            str << char
+        end
+    end
+    s[0].downcase + str
+end
