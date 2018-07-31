679213d820b5988dfc02b9d8e8d02f9ec0c98394
Neil Gewirtz <ngewirtz86@gmail.com>
2018-07-30 20:34:35 -0400

    cleanup dead code

--- proxy.js
+++ proxy.js
@@ -9,12 +9,3 @@ let x = {};
 p['a'] += 1;
 x['a'] += 1;
 console.log(p['a'], x['a']);
-
-// var handler = {
-//   get: function(target, name) {
-//     console.log(target[name])
-//     return name in target ? target[name] : 0;
-// }};
-// var p = new Proxy({}, handler);
-// p['a'] += 1;
-// console.log(p.a, p.b);
