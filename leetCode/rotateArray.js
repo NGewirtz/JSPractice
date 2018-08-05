8b0837ccdbfe7a3ff600bd17500377f5787d3897
Neil Gewirtz <ngewirtz86@gmail.com>
2018-08-04 22:36:53 -0400

    remove consolelog

--- leetCode/rotateArray.js
+++ leetCode/rotateArray.js
@@ -2,8 +2,6 @@ var rotate = function(nums, k) {
   return nums.slice(k) + nums.slice(0, k);
 };
 
-console.log(rotate([-1,-100,3,99], 2));
-
 var rotateInPlace = function(nums, k) {
   for (let i = 0; i < k; i++) {
     nums.unshift(nums.pop());
