e552161062a4290be792633c1e9f1a3ce6d6c005
Neil Gewirtz <ngewirtz86@gmail.com>
2018-11-06 16:53:18 -0500

    closure

--- closure.js
+++ closure.js
@@ -16,3 +16,18 @@ let passwordProtectedSecret = takeSecret("I like doritos");
 console.log(passwordProtectedSecret);
 console.log(passwordProtectedSecret("sadsf"));
 console.log(passwordProtectedSecret("password"));
+
+
+//Ex2
+
+function closed(c) {
+  console.log(c);
+}
+
+function test() {
+  const c = "this worked";
+  return () => closed(c);
+}
+
+let first = test();
+first();
