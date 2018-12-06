f03ff8a1b63ee0b1a766ef7fbf0d7cd7409d3f3b
Neil Gewirtz <ngewirtz86@gmail.com>
2018-12-06 17:46:30 -0500

    almostinc

---
+++ almostInc.js
@@ -0,0 +1,24 @@
+function almostIncreasingSequence(sequence) {
+  let currEl = sequence[0];
+  let firstDown = null;
+  let toSlice = 1;
+  let ans = true;
+  if (currEl > sequence[1]) {
+    toSlice = 2;
+    currEl = sequence[1];
+    firstDown = sequence[0];
+  }
+  sequence.slice(toSlice).forEach(el => {
+    if ( el > currEl) {
+      currEl = el;
+    }else if (firstDown) {
+      ans = false;
+    }else {
+      let prevEl = sequence[sequence.indexOf(currEl) - 1];
+      let lower = (el < currEl) && (el > prevEl) ? el : currEl;
+      firstDown = lower;
+      currEl = lower;
+    }
+  });
+  return ans;
+}
